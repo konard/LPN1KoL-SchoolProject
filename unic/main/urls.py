0b54044a07577f57534fd20f3ef17672b9c41e62
LPN1KoL <nolsufa@gmail.com>
2026-01-17 13:21:30 +1000

    second commit

--- unic/main/urls.py
+++ unic/main/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', main),
+    path('favicon.ico', favicon),
 ]
--- unic/main/views.py
+++ unic/main/views.py
@@ -1,4 +1,11 @@
+from django.http import FileResponse
 from django.shortcuts import render
+
+
 
 def main(request):
     return render(request, 'main.html')
+
+
+def favicon(request):
+    return FileResponse(open('static/imgs/logo.svg', 'rb'))
